7022ad95d9a35d94976d8ef0ac041531ffbcd213
Suchit-inside <inside.suchit@gmail.com>
2025-11-16 16:31:29 +0530

    Hero Section with ComingSoon Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
-// import NavBar from './sections/NavBar';
-// import Hero from './sections/Hero';
+import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
 // import FirstVideo from './sections/FirstVideo';
 // import Jason from './sections/Jason';
 // import SecondVideo from './sections/SecondVideo';
@@ -16,10 +16,10 @@ gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
     <main>
-      {/* <NavBar />
-      <Hero />
+       <NavBar />
+        <Hero />
 
-      <FirstVideo />
+    {/*   <FirstVideo />
       <Jason />
 
       <SecondVideo />
